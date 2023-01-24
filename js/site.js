347e4fdbaa6f384bd0c8e935bce298d4dffa137c
Preytorkamen <101070557+Preytorkamen@users.noreply.github.com>
2023-01-23 19:49:40 -0500

    Javascript Implementation Progress

--- js/site.js
+++ js/site.js
@@ -1,4 +1,28 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+
+
+//Get the values from the Page
+function getValues(){
+    //Get values from page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    //Call generateNumbers
+    let numbers = generateNumbers(startValue, endValue);
+
+    //Call displayNumbers
+}
+
+//Generate numbers from the startValue to the endValue
+function generateNumbers(sValue, eValue){
+    let numbers= [];
+
+    //Get all numbers from start to end
+    for (let i=sValue; i < eValue; i++) {
+       numbers.push(i) 
+    }
+    return numbers;
+}
+
+//Display the numbers and mark the even numbers bold
+function displayNumbers(){
+
 }
